c9265d26ae64ee1a18906d80a4cf2055976c4357
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-12 12:52:07 +0530

    deploy error fixed

--- src/app/_actions.ts
+++ src/app/_actions.ts
@@ -1,49 +1,50 @@
-'use server'
+"use server";
 
-import { z } from 'zod'
-import { Resend } from 'resend'
-import { ContactFormSchema, FormDataSchema } from '@/components/lib/schema'
-import ContactFormEmail from '../../react-email-starter/emails/contact-form'
+import { z } from "zod";
+import { Resend } from "resend";
+import {
+	ContactFormSchema,
+	FormDataSchema,
+} from "@/components/lib/schema";
+import ContactFormEmail from "../../react-email-starter/emails/contact-form";
 
-
-type Inputs = z.infer<typeof FormDataSchema>
-
+type Inputs = z.infer<typeof FormDataSchema>;
 
 export async function addEntry(data: Inputs) {
-    const result = FormDataSchema.safeParse(data)
-  
-    if (result.success) {
-      return { success: true, data: result.data }
-    }
-  
-    if (result.error) {
-      return { success: false, error: result.error.format() }
-    }
-  }
+	const result = FormDataSchema.safeParse(data);
 
-  type ContactFormInputs = z.infer<typeof ContactFormSchema >
-const resend = new Resend(process.env.RESEND_API_KEY)
+	if (result.success) {
+		return { success: true, data: result.data };
+	}
+
+	if (result.error) {
+		return { success: false, error: result.error.format() };
+	}
+}
+
+type ContactFormInputs = z.infer<typeof ContactFormSchema>;
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendEmail(data: ContactFormInputs) {
-    const result = ContactFormSchema.safeParse(data)
+	const result = ContactFormSchema.safeParse(data);
 
-    if (result.success) {
-        const { fullName, email, message } = result.data;
-        try {
-            const data = await resend.emails.send({
-                from: 'noreply@parmjeetmishra.com',
-                to: 'iamparmjeetmishra@gmail.com',
-                reply_to: email,
-                subject: 'Contact Form Submission',
-                text: `Name: ${fullName}\nEmail: ${email}\nMessage: ${message}`,
-                react: ContactFormEmail({ fullName, email, message })
-            })
-            return { success: true, data }
-        } catch (error) {
-            return {success: false, error}
-        }
-    }
-    if (result.error) {
-        return {success: false, error: result.error.format()}
-    }
+	if (result.success) {
+		const { fullName, email, message } = result.data;
+		try {
+			const data = await resend.emails.send({
+				from: "noreply@parmjeetmishra.com",
+				to: "iamparmjeetmishra@gmail.com",
+				// reply_to: email,
+				subject: "Contact Form Submission",
+				text: `Name: ${fullName}\nEmail: ${email}\nMessage: ${message}`,
+				react: ContactFormEmail({ fullName, email, message }),
+			});
+			return { success: true, data };
+		} catch (error) {
+			return { success: false, error };
+		}
+	}
+	if (result.error) {
+		return { success: false, error: result.error.format() };
+	}
 }
