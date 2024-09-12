484096174218e2f701ce4ac6c740c55ccb66e685
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-12 11:54:51 +0530

    New projects added

--- src/app/(pages)/Projects/page.jsx
+++ src/app/(pages)/Projects/page.jsx
@@ -3,6 +3,9 @@ import Parmjeet from '/public/Images/parmjeetmishra.jpg'
 import Amarjeet from '/public/Images/amarjeetmishra.jpg'
 import NextEmail from '/public/Images/NextEmail.jpg'
 import NextLogin from '/public/Images/NextOtpLogin.jpg'
+import NextJobs from '/public/Images/NextJobs.jpg'
+import PetCare from '/public/Images/PetCare.jpg'
+import Evento from '/public/Images/evento.jpg'
 
 import ProjectCard from '/src/components/ui/ProjectCard'
 //import TagBtn from '/src/components/ui/TagBtn'
@@ -50,6 +53,30 @@ export default function Page() {
                         desc="Nextjs Login app with OTP. How UI works"
                         tags='Nextjs | React | useRef | useEffect | useState |'
                         git='https://github.com/iamparmjeetmishra/NextOtpLogin'
+                    />
+                    <ProjectCard
+                        src={NextJobs}
+                        href='https://nextjobs.parmjeetmishra.com/'
+                        title='nxtRmtJobs'
+                        desc="nxtRmtJobs - Job Portal with with API fetching and caching"
+                        tags='React-vite App | useState | React Query | Components Structure | useContext | Custom Hooks | Popups | Search and Filtering | LocalStorage'
+                        git='https://github.com/iamparmjeetmishra/NxtRmtDev'
+                    />
+                    <ProjectCard
+                        src={PetCare}
+                        href='https://petcare.parmjeetmishra.com/'
+                        title='PetCare'
+                        desc="PetCare-Manage your pet daycare with ease"
+                        tags='NextJS | TailwindCSS | Components | Middleware | NextAuth | Postgres DB | Prisma ORM | Typescript | React-Query | Toast | Stripe | Webhooks'
+                        git='https://github.com/iamparmjeetmishra/PetCare'
+                    />
+                    <ProjectCard
+                        src={Evento}
+                        href='https://evento.parmjeetmishra.com/'
+                        title='Evento'
+                        desc="Evento - Search your events"
+                        tags='NextJS | TailwindCSS | Components | Postgres DB | Prisma ORM | Typescript | React-Query | Zod'
+                        git='https://github.com/iamparmjeetmishra/EventoV2'
                     />
                 </main>
             </section>
